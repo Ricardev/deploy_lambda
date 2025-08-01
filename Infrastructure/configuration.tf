892713fad005b3d9e5c867e431f80583101a1b38
Ricardo Alexandre <ricardo.alexandresa@outlook.com>
2025-08-01 15:48:07 -0300

    laga laga

---
+++ Infrastructure/configuration.tf
@@ -0,0 +1,13 @@
+﻿provider "aws" {
+  region = "sa-east-1"
+}
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
---
+++ LambdaService/LambdaEntrypoint.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace LambdaService;
+
+public class LambdaEntrypoint : APIGatewayProxyFunction
+{
+    protected override void Init(IWebHostBuilder builder)
+    {
+        builder.UseStartup<Startup>();
+        base.Init(builder);
+    }
+}
